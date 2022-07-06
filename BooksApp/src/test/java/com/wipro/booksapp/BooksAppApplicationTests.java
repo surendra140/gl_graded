b72120cfc2b09f49146a449764bd13cd404fa438
Surendra <surendrabankar218@gmail.com>
2022-07-06 15:58:35 +0530

    project added

---
+++ BooksApp/src/test/java/com/wipro/booksapp/BooksAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wipro.booksapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
